7f1b40613ad2659bb7e4d8a7893cd513f8a74704
SUYEON KIM <waterkite94@gmail.com>
2024-07-17 20:27:27 +0900

    [Chore] set ktlint

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,8 @@ plugins {
     id("io.spring.dependency-management") version "1.1.5"
     kotlin("jvm") version "1.9.24"
     kotlin("plugin.spring") version "1.9.24"
+
+    id("org.jlleitschuh.gradle.ktlint") version "12.1.1"
 }
 
 group = "io.waterkite94.stalk"
@@ -21,8 +23,13 @@ repositories {
 extra["springCloudVersion"] = "2023.0.3"
 
 dependencies {
+    // base config
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+
+    // spring cloud
     implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-server")
+
+    // test
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
@@ -42,4 +49,10 @@ kotlin {
 
 tasks.withType<Test> {
     useJUnitPlatform()
+}
+
+tasks {
+    jar {
+        enabled = false
+    }
 }
